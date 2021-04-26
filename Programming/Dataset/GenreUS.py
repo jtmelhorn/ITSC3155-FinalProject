d716db8d340c5555aeaf92723ece95ea157ca2cf
Justin <jtmelhorn@gmail.com>
2021-04-26 18:49:05 -0400

    filter by other countries

--- Programming/Dataset/GenreUS.py
+++ Programming/Dataset/GenreUS.py
@@ -1,25 +1,34 @@
 import pandas as pd
 import plotly.graph_objs as go
 import plotly.offline as pyo
+import plotly.express as px
 # Load CVS file from Dataset folder
 
-def showGenreUS():
+def showGenre(country):
 
     df = pd.read_csv('netflix_titles.csv')
 
-    NonUSDF = df[df['country'] != 'United States'].index
+    NonUSDF = df[df['country'] != country].index
 
     df.drop(NonUSDF, inplace=True)
 
+    df['listed_in'] = df['listed_in'].str.split(', ')
+    df = df.explode('listed_in').reset_index(drop=True)
+    cols = list(df.columns)
+    cols.append(cols.pop(cols.index('title')))
+    df = df[cols]
     newdf = pd.value_counts(df['listed_in'], sort=True)[0:10]
 
     newdf = pd.DataFrame({'genre':newdf.index, 'count':newdf.values})
 
     newdf = newdf.sample(frac=1)
 
-    data = [go.Scatter(x=newdf['genre'],y=newdf['count'], mode='lines',name='Trend')]
 
-    layout = go.Layout(title='Frequency of top 10 genres on Netflix in the US', xaxis_title="Genres", yaxis_title="Number of TV shows and Movies per genre")
 
-    fig = go.Figure(data=data,layout=layout)
+    fig = px.pie(newdf, values='count', names='genre', title='Most popular genre in ' + country)
+
     pyo.plot(fig,filename='USGenreLineChart.html')
+
+
+
+showGenre('France')
